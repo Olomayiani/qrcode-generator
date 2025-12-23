0316ed95855bbfb466288a8f9bba989c3faa9e41
Olomayiani <nanaolomaiyani@gmail.com>
2025-12-23 18:49:18 +0300

    qr code

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import qrcode
 
 qr = qrcode.QRCode(
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
     version=4,
-    box_size=10,
-    border=4,
+    box_size=12,
+    border=10,
 )
 
-qr.add_data("https://www.w3schools.com/python/python_class_methods.asp")
+qr.add_data("http.//www.github.com")
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="red", back_color="white")
-img.save("mycode.png")
+img.save("advance.png")
